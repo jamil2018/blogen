cf88d2f8c3bf575afefe80b695ad42e183e00e2c
Hasnat Jamil <hasnatjamil2018@gmail.com>
2021-06-21 00:21:00 +0600

    changeset: 1. setup frontend project boilerplate 2. setup frontend packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from "@material-ui/core";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Typography variant="h1">Hello world</Typography>
+      <Typography variant="body1" component="p">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+        error necessitatibus dolor, quos eum ratione. Dolores, fugiat incidunt!
+        Sit delectus rem sequi eaque a non?
+      </Typography>
+    </>
   );
-}
+};
 
 export default App;
